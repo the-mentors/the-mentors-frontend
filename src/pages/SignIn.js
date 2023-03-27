d6080f0af6f8be4ac3265fcbaa635545ae225582
WoojinDeve <jwj060111@gmail.com>
2023-03-27 11:11:28 +0900

    docs: Issue와 PR 템플릿 생성

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../public/api/elements';
 import { emailCheckReg } from '../public/shared/emailCheck';
